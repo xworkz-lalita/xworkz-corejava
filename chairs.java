cfeb69c3a754dcd300b63b6829f4909059136f30
xworkz-lalita <lalita.xworkz@gmail.com>
2020-11-25 16:47:01 +0530

    commiting datatypes java files

---
+++ chairs.java
@@ -0,0 +1,11 @@
+class NoOfChairs
+{
+public static void main(String a[])
+{
+byte noofchairs = 12;
+byte age = -120;
+byte height = 14;
+System.out.println(noofchairs);
+System.out.print(age);
+System.out.printf("%d",height);
+}}
