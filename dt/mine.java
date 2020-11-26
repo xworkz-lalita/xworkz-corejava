4e7b637f61598c20087ee7d054299f0c6643f6e6
xworkz-lalita <lalita.xworkz@gmail.com>
2020-11-26 19:49:12 +0530

    adding dt java files

---
+++ dt/mine.java
@@ -0,0 +1,8 @@
+class names
+{public static void main(String a[])
+{
+char name='l';
+char special='$';
+System.out.print(name);
+System.out.print(special);
+}}
---
+++ dt/tv2.java
@@ -0,0 +1,10 @@
+class Channels
+{
+static String name="TV9";
+static String area="vijaynagar";
+public static void main(String a[])
+{
+System.out.println(area);
+System.out.println(name);
+}
+}
