5a888225409b2cccff61fc08f1e399fb58d80449
xworkz-lalita <lalita.xworkz@gmail.com>
2020-12-05 14:31:14 +0530

    adding eclipse adding files

---
+++ add/src/com/xworkz/blockex/Restaurent.java
@@ -0,0 +1,22 @@
+package com.xworkz.blockex;
+
+public class Restaurent {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Restaurent rest =new Restaurent();
+		System.out.println(rest);
+		
+	}
+
+	{
+		System.out.println("welcome");
+	}static
+	{
+	String name="mayuri";
+	String address="bellary";
+System.out.println(name);
+System.out.println(address);
+}
+}
---
+++ add/src/com/xworkz/rest/Restaurent.java
@@ -0,0 +1,23 @@
+package com.xworkz.rest;
+
+public class Restaurent
+{
+	static int id;
+	public String name;
+	public	String nameOne;
+	public String nameTwo;
+	public String nameThree;
+	public String nameFour;
+	public String nameFive;
+	public String nameSix;
+	public String nameSeven;
+	public String nameEight;
+{
+	id=2345;
+	System.out.println(id);
+	}
+ static void serveFood()
+{
+	System.out.println(id);}
+	
+}
