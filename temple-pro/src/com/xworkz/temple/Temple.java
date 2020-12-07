1da9d4dd2bb2e70821d12acc93c1f846c122addf
xworkz-lalita <lalita.xworkz@gmail.com>
2020-12-08 00:33:56 +0530

    adding java files

---
+++ temple-pro/src/com/xworkz/temple/Temple.java
@@ -0,0 +1,25 @@
+package com.xworkz.temple;
+
+public class Temple 
+{
+	private String name;
+	private String address;
+	public String getName()//no parameter with return type
+
+	{
+		return name;}
+
+	public void setName(String name)//parameter but no return type
+	{
+		this.name=name;
+		}
+	public String getAddress()
+
+	{
+		return address;}
+
+	public void setAddress(String address)
+	{
+		this.address=address;
+		}
+}
---
+++ temple-pro/src/com/xworkz/util/TempleUtil.java
@@ -0,0 +1,15 @@
+package com.xworkz.util;
+
+import com.xworkz.temple.Temple;
+
+public class TempleUtil {
+
+	public static void main(String[] args) {
+		Temple tm=new Temple();
+		tm.setName("Anjaneya temple");
+		tm.setAddress("Rajajinagar");
+		System.out.println(tm.getName()+" "+tm.getAddress());
+
+	}
+
+}
