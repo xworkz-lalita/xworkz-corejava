288dd137332b95811e94cd8d910fcf99ff2d64c3
xworkz-lalita <lalita.xworkz@gmail.com>
2020-12-08 00:26:34 +0530

    adding jav files

---
+++ car-java/gadgets/src/com/xworkz/gadgetsutil/GadgetsUtil.java
@@ -0,0 +1,15 @@
+package com.xworkz.gadgetsutil;
+
+import com.xworkz.gadgets.Gadgets;
+
+public class GadgetsUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gadgets gd=new Gadgets();
+		gd.setId(1232);
+		System.out.println(gd.getId());
+
+	}
+
+}
