9687163530e6987ca1fdf1b824f0bc1bed878dd2
xworkz-lalita <lalita.xworkz@gmail.com>
2020-12-08 00:41:27 +0530

    adding java files

---
+++ music-java/src/com/xworkz/Music.java
@@ -0,0 +1,14 @@
+package com.xworkz;
+
+public class Music 
+ {
+		//ring name;
+		String categories;
+		String musicdirector;
+			void relax()
+		{
+			System.out.println("relaxation");
+		
+	     }
+
+}
---
+++ music-java/src/com/xworkz/Music_Util.java
@@ -0,0 +1,15 @@
+package com.xworkz;
+
+public class Music_Util
+{
+	public static void main(String[] args)
+	{
+		Music music=new Music();
+		//music.name="melody";
+		music.categories="rajesh krishnanan";
+		music.musicdirector=" soothing";
+		music.relax();
+		System.out.println( music.categories + music.musicdirector);	
+	}
+
+}
